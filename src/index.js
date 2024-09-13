bc2fa3732248f961fb2522f72919af8613e291a3
Eyob Hagos <104778355+hadamard-2@users.noreply.github.com>
2024-09-13 14:20:24 +0300

    added some functionalities to the code editor
    - switched from Python to JS for the cube programming language
    - added line numbers, line wrapping and bracket pairing
    - designed and added button for running code
    - user can now run code he/she writes on the code editor

--- src/index.js
+++ src/index.js
@@ -1,13 +1,22 @@
+// Code Editor - CodeMirror 6
 import { EditorState } from "@codemirror/state";
-import { EditorView, keymap } from "@codemirror/view";
+import { EditorView, keymap, lineNumbers } from "@codemirror/view";
 import { defaultKeymap } from "@codemirror/commands";
-import { python } from "@codemirror/lang-python";
+import { javascript } from "@codemirror/lang-javascript";
 import { oneDark } from "@codemirror/theme-one-dark";
+import { closeBrackets } from "@codemirror/autocomplete";
 
 // Create an initial state with some default code
 const initialState = EditorState.create({
-    doc: "def foo():\n    print(123)",
-    extensions: [keymap.of(defaultKeymap), python(), oneDark]
+    doc: "function foo() {\n  console.log(123);\n}\n\nfoo();",
+    extensions: [
+        keymap.of(defaultKeymap),
+        javascript(),
+        oneDark,
+        lineNumbers(),
+        EditorView.lineWrapping,
+        closeBrackets()
+    ]
 });
 
 // Create the editor view and attach it to the DOM element
@@ -15,3 +24,17 @@ const view = new EditorView({
     state: initialState,
     parent: document.getElementById("editor")
 });
+
+function executeCode() {
+    // Get the code from the editor
+    const code = view.state.doc.toString();
+
+    try {
+        eval(code);
+    } catch (error) {
+        console.error("Error executing code:", error);
+    }
+}
+
+
+document.querySelector(".run-button").addEventListener("click", executeCode);
